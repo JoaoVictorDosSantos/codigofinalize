1c180fb6bbba8f07ae99ee4b1188eabc6b1e1f0b
santos.joao@aluno.ifsp.edu.br <joao_97victorsantos@hotmail.com>
2019-02-28 19:22:32 -0300

    Casos de Teste

---
+++ TesteFinalizeDois.java
@@ -0,0 +1,19 @@
+public class TesteFinalizeDois{
+	
+	public static class Teste {
+		
+		 protected void finalize( ) throws Throwable{
+		 super.finalize();
+		 System.out.print("Finalize foi chamada. ");
+		 }
+	
+	}
+	//
+	public static void main(String[ ] args){
+		Teste te = new Teste();
+		 te = new Teste ( );
+		for (int i=1; i <1000000; i++);
+		 		System.out.print("Acabou!");
+	}
+	
+}

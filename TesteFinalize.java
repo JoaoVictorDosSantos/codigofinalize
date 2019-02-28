1af44abe587c67d054bb2689af997956be256915
JoaoVictorDosSantos <47449455+JoaoVictorDosSantos@users.noreply.github.com>
2019-02-28 13:45:50 -0300

    Add files via upload
    Uso inadequado e adequado do método finalize.

---
+++ TesteFinalize.java
@@ -0,0 +1,20 @@
+public class TesteFinalize{
+	
+	public static class Teste {
+		
+		 protected void finalize( ) throws Throwable{
+		 super.finalize();
+		 System.out.print("Finalize foi chamada. ");
+		 }
+	
+	}
+	
+	public static void main(String[ ] args){
+		Teste te = new Teste();
+		 te = new Teste ( );
+		 System.gc();
+		 for (int i=1; i <1000000; i++);
+		 		System.out.print("Acabou!");
+	}
+	
+}
